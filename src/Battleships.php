a49d97016951ac155608d21b141b94a69b234e60
Cole Henley <cole@ournameismud.co.uk>
2018-11-01 10:15:36 +0000

    fix field issue (image fallback)

--- src/Battleships.php
+++ src/Battleships.php
@@ -49,7 +49,7 @@ class Battleships extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '0.0.1';
+    public $schemaVersion = '0.0.2';
 
     // Public Methods
     // =========================================================================
